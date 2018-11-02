8d5cd04fc82cc9af79f24466cb891d952990f3a8
Daniel <danobo@op.pl>
2018-11-02 12:48:20 +0100

    PA05_POT

--- src/com/company/PA05_POT.java
+++ src/com/company/PA05_POT.java
@@ -3,20 +3,21 @@ package com.company;
 import java.util.Scanner;
 
 public class PA05_POT {
-    public void potęga() {
-        System.out.println("Podaj liczby a i b");
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
 
-        Double wynik = Math.pow(a, b);
-        Integer wynik2 = wynik.intValue();
-        String wynikWPostaciStringa = wynik2.toString();
+        public void potęga() {
+            System.out.println("Podaj liczby a i b");
+            Scanner scanner = new Scanner(System.in);
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
 
-        Integer dlugosc = wynikWPostaciStringa.length();
-        char ostatniaCyfra = wynikWPostaciStringa.charAt(dlugosc-1);
-        System.out.println(ostatniaCyfra);
+            Double wynik = Math.pow(a, b);
+            Integer wynik2 = wynik.intValue();
+            String wynikWPostaciStringa = wynik2.toString();
+
+            Integer dlugosc = wynikWPostaciStringa.length();
+            char ostatniaCyfra = wynikWPostaciStringa.charAt(dlugosc-1);
+            System.out.println(ostatniaCyfra);
+
+        }
 
     }
-
-}
